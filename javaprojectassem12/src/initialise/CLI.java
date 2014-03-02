a71336c477feafe0b10a5001cf29e3e19dbecff4
qqsmalleindigoqq <wa_waxd@msn.com>
2014-03-02 16:53:46 +0100

    start CLI

--- javaprojectassem12/src/initialise/CLI.java
+++ javaprojectassem12/src/initialise/CLI.java
@@ -1,10 +1,41 @@
 package initialise;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class CLI {
 
-	public void run() {
-		
-		
+	private BufferedReader reader;
+	private BufferedWriter writer;
+	public CLI(){
+		this.reader= new BufferedReader(new InputStreamReader(System.in));
+		this.writer= new BufferedWriter(new OutputStreamWriter(System.out));
+		this.run();
+	}
+	private void run() {
+
+
+		try {
+			String derp =reader.readLine();
+			writer.write(derp+"succes");
+			writer.flush();
+		} catch (IOException e) {
+			System.out.println("u failed xd");
+		}
+
+	}
+
+	class Communication{
+		public void write(String input) throws IOException{
+			writer.write(input);
+			writer.flush();
+		}
+		public String readInput() throws IOException{
+			return reader.readLine();
+		}
 	}
 
 }
--- javaprojectassem12/src/initialise/Main.java
+++ javaprojectassem12/src/initialise/Main.java
@@ -3,6 +3,6 @@ package initialise;
 public class Main {
 public static void main(String[] args) {
 	CLI cli = new CLI();
-	cli.run();
+	
 }
 }
