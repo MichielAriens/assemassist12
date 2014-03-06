b53d8e34dc90c0f3abeaea9f5ab78d9a4723461d
JeroenCo <jeroen.compagnie@student.kuleuven.be>
2014-03-06 11:08:32 +0100

    Carorder en assemblyline met calendar enzo
    zie hierboven

--- javaprojectassem12/src/logic/assemblyline/AssemblyLine.java
+++ javaprojectassem12/src/logic/assemblyline/AssemblyLine.java
@@ -1,16 +1,15 @@
 package logic.assemblyline;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Calendar;
 
 import logic.car.CarOrder;
-import logic.car.CarPartType;
 import logic.workstation.AccessoriesPost;
 import logic.workstation.CarBodyPost;
 import logic.workstation.DriveTrainPost;
 import logic.workstation.Workstation;
 
 /**
- * Class handeling an assembly line of a car manufacturing company.
+ * Class handling an assembly line of a car manufacturing company.
  */
 
 public class AssemblyLine {
@@ -68,23 +67,15 @@ public class AssemblyLine {
 		carOrders.add(order);
 	}
 
-	public class Schedule {	
-		/**
-		 * 
-		 */
-		private String standardBeginTime;
-		
-		/**
-		 * 
-		 */
-		private String standardEndTime;
+	public class Schedule {
+		// TODO: Misschien variabelen maken voor de begintijd en eindtijd van een werk dag 
 		
 		/**
 		 * 
 		 * @param order
 		 * @return
 		 */
-		public String getEstimatedCompletionTime(CarOrder order){
+		public Calendar getEstimatedCompletionTime(CarOrder order){
 			return order.getEstimatedEndTime();
 		}
 
@@ -95,9 +86,71 @@ public class AssemblyLine {
 			return carOrders.get(numberOfWorkStations);
 		}
 		
-		public String calculateEstimatedEndTime(CarOrder carOrder){
-			if(carOrders.get(carOrders.indexOf(carOrder) - 1).getEstimatedEndTime() != null);
-			return "8008";
+		public Calendar calculateEstimatedEndTime(CarOrder carOrder){
+			if(carOrders.size() > 1){ 
+				Calendar endTimePreviousOrder = carOrders.get(carOrders.indexOf(carOrder) - 1).getEstimatedEndTime(); 
+
+				Calendar endTimeNextOrder = Calendar.getInstance(); 
+				endTimeNextOrder = Calendar.getInstance();
+				endTimeNextOrder.setTime(endTimePreviousOrder.getTime());
+				endTimeNextOrder.add(Calendar.HOUR_OF_DAY, 3); // 3 uur bijvoegen
+
+				Calendar checkDate = Calendar.getInstance(); 
+				checkDate.setTime(endTimePreviousOrder.getTime());
+				checkDate.set(Calendar.HOUR_OF_DAY, 22);
+				checkDate.set(Calendar.MINUTE, 0);
+				if(endTimeNextOrder.before(checkDate)){
+					return endTimeNextOrder;
+				}
+				else{
+					endTimeNextOrder.setTime(endTimePreviousOrder.getTime());
+					endTimeNextOrder.add(Calendar.DAY_OF_WEEK, 1); // add 1 day
+					
+					endTimeNextOrder.set(Calendar.HOUR_OF_DAY, 6);
+					endTimeNextOrder.set(Calendar.MINUTE, 0);
+					endTimeNextOrder.set(Calendar.SECOND, 0);
+					
+					endTimeNextOrder.add(Calendar.HOUR_OF_DAY, 3); // 3 uur bijvoegen
+					return endTimeNextOrder;
+				}
+			}
+			
+			//Als de gegeven carorder de enige carorder is moet naar het huidige uur gekeken worden
+			else{
+				//Current date
+				Calendar endTimeNextOrder = Calendar.getInstance();
+				endTimeNextOrder.getTime();
+				
+				Calendar checkTime0600 = Calendar.getInstance(); 
+				checkTime0600.setTime(endTimeNextOrder.getTime());
+				checkTime0600.set(Calendar.HOUR_OF_DAY, 6);
+				checkTime0600.set(Calendar.MINUTE, 0);
+				checkTime0600.set(Calendar.SECOND, 0);
+				
+				Calendar checkTime2200 = Calendar.getInstance(); 
+				checkTime2200.setTime(endTimeNextOrder.getTime());
+				checkTime2200.set(Calendar.HOUR_OF_DAY, 22);
+				checkTime2200.set(Calendar.MINUTE, 0);
+				checkTime2200.set(Calendar.SECOND, 0);
+				
+				endTimeNextOrder.add(Calendar.HOUR_OF_DAY, 3); // 3 uur bijvoegen
+				
+				// nakijken of het tussen 6 en 8 is
+				if(endTimeNextOrder.after(checkTime0600)){
+					if(endTimeNextOrder.before(checkTime2200)){
+						return endTimeNextOrder;
+					}
+					else{
+						endTimeNextOrder.setTime(checkTime0600.getTime());
+						endTimeNextOrder.add(Calendar.DAY_OF_WEEK, 1); // add 1 day
+						return endTimeNextOrder;
+					}
+				}
+				else{
+					endTimeNextOrder.setTime(checkTime0600.getTime());
+					return endTimeNextOrder;
+				}	
+			}	
 		}
 	}
 }
--- javaprojectassem12/src/logic/car/CarOrder.java
+++ javaprojectassem12/src/logic/car/CarOrder.java
@@ -1,101 +1,70 @@
 package logic.car;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-
-import logic.users.GarageHolder;
 import logic.workstation.Task;
 
-
 public class CarOrder {
-	
-	private final String DATE_FORMAT = "HH:mm dd/MM/yyyy";
-		private String startTime;
-		private String endTime;
-		private String estimatedEndTime;
-		private GarageHolder garageHolder;
-		private DateFormat dateFormat;
-		private CarSpecification carSpecification;
-		private List<Task> tasks = new ArrayList<Task>();
-		
-		public CarOrder(GarageHolder garageHolder, CarSpecification carSpecification){
-			this.carSpecification=carSpecification;
-			this.garageHolder=garageHolder;
-			startTime = this.getTime();
-			this.dateFormat= new SimpleDateFormat(DATE_FORMAT);
+
+	private Calendar startTime;
+	private Calendar endTime;
+	private Calendar estimatedEndTime;
+	private CarSpecification carSpecification;
+	private List<Task> tasks = new ArrayList<Task>();
+
+	public CarOrder(CarSpecification carSpecification, Calendar startTime){
+		this.carSpecification = carSpecification;
+		this.startTime = startTime;
+		buildTasks();
+	}
+
+	/**
+	 * Build a list of tasks to be done. For use on the production line.
+	 */
+	private void buildTasks() {
+		for(CarPart part : this.getCarSpecification().getParts()){
+			this.tasks.add(new Task(part));
 		}
-		
-		
-		/**
-		 * Build a list of tasks to be done. For use on the production line.
-		 */
-		private void buildTasks() {
-			for(CarPart part : this.getCarSpecification().getParts()){
-				this.tasks.add(new Task(part));
+	}
+
+	public List<Task> getTasks(){
+		return this.tasks;
+	}
+
+	public Calendar getStartTime() {
+		return startTime;
+	}
+
+	public Calendar getEndTime() {
+		return endTime;
+	}
+
+	public Calendar getEstimatedEndTime() {
+		return estimatedEndTime;
+	}
+
+	public CarSpecification getCarSpecification() {
+		return carSpecification;
+	}
+
+	public void setEstimatedEndTime(Calendar time){
+		this.estimatedEndTime = time;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean done(){
+		boolean retVal = true;
+		for(Task task : this.tasks){
+			if(!task.isComplete()){
+				retVal = false;
+				break;
 			}
 		}
-
-		
-		public List<Task> getTasks(){
-			return this.tasks;
-		}
-
-		
-		/**
-		 * 
-		 * @return
-		 */
-		public boolean done(){
-			boolean retVal = true;
-			for(Task task : this.tasks){
-				if(!task.isComplete()){
-					retVal = false;
-					break;
-				}
-			}
-			return retVal;
-		}
-		
-
-		public String getStartTime() {
-			return startTime;
-		}
-
-		public String getEndTime() {
-			return endTime;
-		}
-
-		public String getEstimatedEndTime() {
-			return estimatedEndTime;
-		}
-
-		public CarSpecification getCarSpecification() {
-			return carSpecification;
-		}
-
-		private String getTime(){
-			Calendar cal = Calendar.getInstance();
-			return dateFormat.format(cal.getTime());
-		}
-		
-		public boolean setEstimatedEndTime(String time){
-			if(this.isValidTime(time)){
-				this.estimatedEndTime=time;
-				return true;
-			}
-			return false;
-		}
-
-		private boolean isValidTime(String time) {
-			try{
-				dateFormat.parse(time);
-				return true;
-			}catch(Exception e){
-				return false;
-			}
-		}
+		return retVal;
+	}
 }
 
