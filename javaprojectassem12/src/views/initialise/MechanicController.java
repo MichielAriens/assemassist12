2202213b5de1ae264f6acf321a38415c183003ff
JorenVandenBrande <joren_vdb@hotmail.be>
2014-03-09 14:00:21 +0100

    fappityfoop

--- javaprojectassem12/src/views/initialise/MechanicController.java
+++ javaprojectassem12/src/views/initialise/MechanicController.java
@@ -1,5 +1,7 @@
 package views.initialise;
 
+import java.util.ArrayList;
+
 import logic.users.Mechanic;
 
 public class MechanicController extends UserController{
@@ -15,6 +17,19 @@ public class MechanicController extends UserController{
 		if(this.mechanic != null)
 			return this.mechanic.getUserName();
 		return null;
+	}
+	
+	public ArrayList<String> getWorkStations(){
+		//vraag mechanic class voor lijst van workstations, mechanic vraagt volledige lijst aan carManufacturingCompany
+		//zo kunnen we in de toekomst ook eenvoudig uitbreiden naar mechanics die niet aan alle workstations mogen werken
+		
+		//vorm deze lijst om naar strings en return
+		return null;
+	}
+	
+	public void setWorkStation(String name){
+		//vraag mechanic om de lijst van workstations, kies workstation dat overeen komt met de naam en roep
+		//laat de mechanic zijn current
 	}
 
 }
