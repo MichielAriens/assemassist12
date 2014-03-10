cd478a1f53669d8ac5858ea4a06629ee5077850c
JeroenCo <jeroen.compagnie@student.kuleuven.be>
2014-03-10 09:33:59 +0100

    documentation en getscheduleoverview voor car manuffacjdagk ,vd

--- javaprojectassem12/src/logic/users/CarManufacturingCompany.java
+++ javaprojectassem12/src/logic/users/CarManufacturingCompany.java
@@ -2,19 +2,45 @@ package logic.users;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import logic.assemblyline.AssemblyLine;
 import logic.car.CarOrder;
 import logic.workstation.Workstation;
 
+/**
+ *
+ */
 public class CarManufacturingCompany {
-	private HashMap<String,User> users;
-	ArrayList<String> validNamesGarage = new ArrayList<String>();
-	ArrayList<String> validNamesManager = new ArrayList<String>();
-	ArrayList<String> validNamesMechanic = new ArrayList<String>();
 	
+	/**
+	 * HashMap holding all the users of this system together with their user names.
+	 */
+	private HashMap<String,User> users;
+	
+	/**
+	 * List of all valid names for garage holders.
+	 */
+	private ArrayList<String> validNamesGarageHolders = new ArrayList<String>();
+	
+	/**
+	 * List of all valid names for managers.
+	 */
+	private ArrayList<String> validNamesManager = new ArrayList<String>();
+	
+	/**
+	 * List of all valid names for car mechanic.
+	 */
+	private ArrayList<String> validNamesMechanic = new ArrayList<String>();
+	
+	/**
+	 * Variable holding the assembly line of this car manufacturing company.
+	 */
 	private AssemblyLine assemblyLine;
 	
+	/**
+	 * Makes a new car manufacturing company.
+	 */
 	public CarManufacturingCompany(){
 		this.assemblyLine = new AssemblyLine();
 		this.users = new HashMap<String,User>();
@@ -22,13 +48,26 @@ public class CarManufacturingCompany {
 		
 	}
 
+	/**
+	 * Initializes the users of this car manufacturing company.
+	 */
 	private void initialise() {
-		validNamesGarage.add("Jex");
+		validNamesGarageHolders.add("Jex");
 		validNamesManager.add("Wander");
 		validNamesMechanic.add("Joren");
-		validNamesGarage.add("Michiel");
+		validNamesGarageHolders.add("Michiel");
 	}
 
+	/**
+	 * Checks if the given user name is valid and returns the corresponding user 
+	 * @param userName		The user name used to login.
+	 * @return	Returns the user corresponding to the given user name otherwise if:
+	 * 			- the list of users contains the given user name,
+	 * 			- the list of valid manager names contains the given user name,
+	 * 			- the list of valid garage holders names contains the given user name or
+	 * 			- the list of valid mechanic names contains the given user name.
+	 * 			Returns null otherwise.
+	 */
 	public User logIn(String userName){
 		if(users.containsKey(userName)){
 			return users.get(userName);
@@ -38,7 +77,7 @@ public class CarManufacturingCompany {
 			users.put(userName, user);
 			return user;
 		}
-		if(validNamesGarage.contains(userName)){
+		if(validNamesGarageHolders.contains(userName)){
 			GarageHolder user = new GarageHolder(this, userName);
 			users.put(userName, user);
 			return user;
@@ -50,13 +89,27 @@ public class CarManufacturingCompany {
 		}
 		return null;
 	}
+	
+	/**
+	 * Adds the given order to the assembly line if the given order is not null.
+	 * @param order		the order which needs to be added to the assembly line
+	 */
 	public void addOrder(CarOrder order) {
 		if(order != null)
 			this.assemblyLine.addCarOrder(order);
 	}
 	
+	/**
+	 * Method to return the list of workstations of the assembly line 
+	 * of this car manufacturing company.
+	 * @return	the workstations of the assembly line of this car manufacturing company
+	 */
 	public Workstation[] getWorkStations(){
 		return this.assemblyLine.getWorkStations();
+	}
+	
+	public List<String> getScheduleOverview(){
+		return this.assemblyLine.getScheduleOverview();
 	}
 	
 	
