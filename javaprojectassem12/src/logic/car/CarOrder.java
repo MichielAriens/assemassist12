a26bb7dc5eb7fc800c62c9e91246504a197897c0
JeroenCo <jeroen.compagnie@student.kuleuven.be>
2014-03-10 10:23:07 +0100

    documentationnes carorder

--- javaprojectassem12/src/logic/car/CarOrder.java
+++ javaprojectassem12/src/logic/car/CarOrder.java
@@ -7,21 +7,50 @@ import org.joda.time.DateTime;
 
 import logic.workstation.Task;
 
+/**
+ * Class used to describe a car order.
+ */
 public class CarOrder {
 
+	/**
+	 * The time this car order was created.
+	 */
 	private DateTime startTime;
+	
+	/**
+	 * The time this car order was completed.
+	 */
 	private DateTime endTime;
+	
+	/**
+	 * An estimated time for when this car order will be finished.
+	 * If the car order is finished this will be the same as the end time.
+	 */
 	private DateTime estimatedEndTime;
+	
+	/** 
+	 * The specifications for this car order.
+	 */
 	private CarSpecification carSpecification;
+	
+	/**
+	 * List of tasks that need to be completed to complete this car order.
+	 */
 	private List<Task> tasks = new ArrayList<Task>();
 
+	/**
+	 * Creates a car order and initializes the car specification to the given car
+	 * specification. Builds the tasks that need to be completed for this car order to
+	 * be completed.
+	 * @param carSpecification	The car specification defining this car order.
+	 */
 	public CarOrder(CarSpecification carSpecification){
 		this.carSpecification = carSpecification;
 		buildTasks();
 	}
 
 	/**
-	 * Build a list of tasks to be done. For use on the production line.
+	 * Builds a list of tasks that need to be completed for this car order to be completed.
 	 */
 	private void buildTasks() {
 		for(CarPart part : this.getCarSpecification().getParts()){
@@ -29,42 +58,75 @@ public class CarOrder {
 		}
 	}
 
+	/**
+	 * Returns the list of tasks that need to be completed to complete this car order.
+	 * @return	The list of tasks.
+	 */
 	public List<Task> getTasks(){
 		return this.tasks;
 	}
 
+	/**
+	 * Returns the time this car order was created.
+	 * @return The time this car order was created.
+	 */
 	public DateTime getStartTime() {
 		return startTime;
-	}
-
-	public DateTime getEndTime() {
-		return endTime;
-	}
-
-	public DateTime getEstimatedEndTime() {
-		return estimatedEndTime;
-	}
-
-	public CarSpecification getCarSpecification() {
-		return carSpecification;
-	}
-
-	public void setEstimatedEndTime(DateTime time){
-		this.estimatedEndTime = time;
-	}
-	
-	public void setEndTime(DateTime time){
-		this.endTime = time;
-		this.estimatedEndTime = endTime;
-	}
-
-	public void setStartTime(DateTime startTime) {
-		this.startTime = startTime;
 	}
 
 	/**
 	 * 
 	 * @return
+	 */
+	public DateTime getEndTime() {
+		return endTime;
+	}
+
+	/**
+	 * Returns the time this car order was completed.
+	 * If the car order is finished this will be the same as the end time.
+	 * @return The time this car order was completed.
+	 */
+	public DateTime getEstimatedEndTime() {
+		return estimatedEndTime;
+	}
+
+	/**
+	 * An estimated time for when this car order will be finished.
+	 * @return An estimated time for when this car order will be finished.
+	 */
+	public CarSpecification getCarSpecification() {
+		return carSpecification;
+	}
+
+	/**
+	 * 
+	 * @param time	Time to set
+	 */
+	public void setEstimatedEndTime(DateTime time){
+		this.estimatedEndTime = time;
+	}
+	
+	/**
+	 * 
+	 * @param time
+	 */
+	public void setEndTime(DateTime time){
+		this.endTime = time;
+		this.estimatedEndTime = endTime;
+	}
+
+	/**
+	 * 
+	 * @param startTime
+	 */
+	public void setStartTime(DateTime startTime) {
+		this.startTime = startTime;
+	}
+
+	/**
+	 * Checks if this car order is completed by checking if all its tasks are completed.
+	 * @return 
 	 */
 	public boolean done(){
 		boolean retVal = true;
@@ -77,6 +139,9 @@ public class CarOrder {
 		return retVal;
 	}
 	
+	/**
+	 * 
+	 */
 	@Override
 	public String toString(){
 		String str;
