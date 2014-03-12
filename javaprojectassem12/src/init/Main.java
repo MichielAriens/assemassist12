631dd4468b334de3880af438852ca5da8348acf0
JorenVandenBrande <joren_vdb@hotmail.be>
2014-03-12 11:07:07 +0100

    main documented nice

--- javaprojectassem12/src/init/Main.java
+++ javaprojectassem12/src/init/Main.java
@@ -1,11 +1,18 @@
 package init;
 
-
 import logic.users.CarManufacturingCompany;
 import views.UI;
 import controllers.AssemAssistController;
 
+/**
+ * A class used to start up the AssemAssist system.
+ */
 public class Main {
+	
+	/**
+	 * The main method that starts up the AssemAssist system.
+	 * @param args Not used.
+	 */
 	public static void main(String[] args) {
 		CarManufacturingCompany company = new CarManufacturingCompany();
 		AssemAssistController controller = new AssemAssistController(company);
