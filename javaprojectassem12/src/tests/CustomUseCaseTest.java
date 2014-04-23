7a262bfa420c8bbaeb7ec5eee743b839cbbfe0b3
Michiel Ariens <michiel.ariens@student.kuleuven.be>
2014-04-23 14:09:06 +0200

    custom manager

---
+++ javaprojectassem12/src/tests/CustomUseCaseTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import static org.junit.Assert.*;
+import logic.users.CarManufacturingCompany;
+import logic.users.CustomsManager;
+
+import org.junit.Test;
+
+import controllers.AssemAssistController;
+import controllers.CustomsManagerController;
+
+public class CustomUseCaseTest {
+
+	@Test
+	public void mainSuccessScenario() {
+		AssemAssistController controller = new AssemAssistController(new CarManufacturingCompany());
+		
+		//Precondition: the user logs in
+		CustomsManagerController custCont = (CustomsManagerController) controller.logIn("cust");
+		
+		//1. The user wants to order a single task
+		//2. The system shows the list of available tasks.
+		//	2.a. The user chooses the type of task from:
+			assertTrue(custCont.getAvailableTypes().contains("Colour: 1"));
+			assertTrue(custCont.getAvailableTypes().contains("Seats: 2"));
+		//	2.b. The user chooses a specific task. (Here we choose from type 1)
+			assertTrue(custCont.getAvailableOptions("Colour").toString().equals("[Red: 1, Blue: 2, Black: 3, White: 4, Green: 5, Yellow: 6]"));
+			custCont.choosePart("Red");
+		//3. The user has selected the task he wants to order (Colour: RED)
+		//4. The system asks for a deadline
+		//5. The user enters the required details
+			custCont.chooseDeadLine("10-12-2050 10:30");
+		//6. System stores the order and updates the schedule.
+			String orderInfo = custCont.placeOrder();
+		//7. The system presents an estimated completion date
+			assertTrue(orderInfo.contains("01-01-2014 09:00"));
+			
+			
+		
+	}
+
+}
