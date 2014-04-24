e4666b4f791d7646f818804ae942f2be1defe559
Michiel Ariens <michiel.ariens@student.kuleuven.be>
2014-04-24 10:18:37 +0200

    een s

--- javaprojectassem12/src/logic/assemblyline/FifoStrategy.java
+++ javaprojectassem12/src/logic/assemblyline/FifoStrategy.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import logic.car.Order;
 
 /**
- * A class that uses FIFO strategy to add orders and refactor queues.
+ * A class that uses FIFO strategy to add orders and refactors queues.
  */
 public class FifoStrategy extends SchedulingStrategy{
 
