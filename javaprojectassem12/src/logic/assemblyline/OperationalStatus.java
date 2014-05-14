f1a509a88710f9c0fe91b4f749e21f0d4addde1a
qqsmalleindigoqq <wa_waxd@msn.com>
2014-05-14 14:24:08 +0200

    fgdfgdfgd

--- javaprojectassem12/src/logic/assemblyline/OperationalStatus.java
+++ javaprojectassem12/src/logic/assemblyline/OperationalStatus.java
@@ -1,17 +1,24 @@
 package logic.assemblyline;
 
 public enum OperationalStatus {
-	OPERATIANAL("Operational"), 
-	MAINTENANCE("Maintenance"), 
-	BROKEN("Broken");
+	OPERATIANAL("Operational",0), 
+	MAINTENANCE("Maintenance",240), 
+	BROKEN("Broken",0);
 	
 	/**
 	 * The full name of this operational status.
 	 */
 	public final String fullName;
 	
-	OperationalStatus(String name){
+	public final int time;
+	
+	OperationalStatus(String name, int time){
 		fullName = name;
+		this.time = time;
+	}
+	
+	public int getTime(){
+		return this.time;
 	}
 	
 	/**
