9727ee44411ad2b971998b9021e8636bc451a9c1
Michiel Ariens <michiel.ariens@student.kuleuven.be>
2014-05-14 15:37:00 +0200

    sjqkdqd

--- javaprojectassem12/src/logic/assemblyline/AssemblyLineScheduler.java
+++ javaprojectassem12/src/logic/assemblyline/AssemblyLineScheduler.java
@@ -1,39 +1,33 @@
 package logic.assemblyline;
 
-import java.util.Collection;
+import interfaces.Printable;
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import logic.car.Order;
+import logic.car.VehicleModel;
 import logic.workstation.WorkstationChainBuilder;
 import logic.workstation.WorkstationDirector;
 import logic.workstation.WorkstationDirectorA;
+import logic.workstation.WorkstationDirectorB;
 
 import org.joda.time.DateTime;
 
 public class AssemblyLineScheduler {
 	
-	private Collection<AssemblyLine> assemblyLines;
+	private Set<AssemblyLine> assemblyLines;
+	private DateTime currentTime;
 	
 	/**
 	 * 
 	 */
 	public AssemblyLineScheduler(){
 		assemblyLines = new HashSet<>();
-	}
-	
-	/**
-	 * 
-	 * @param lines
-	 */
-	public void addAllLines(Collection<AssemblyLine> lines){
-		assemblyLines.addAll(lines);
-	}
-	
-	private void addLine(AssemblyLine line){
-		assemblyLines.add(line);
+		initializeAssemblylines();
 	}
 	
 	/**
@@ -65,18 +59,83 @@ public class AssemblyLineScheduler {
 		best.addOrder(order);
 	}
 	
+	/**
+	 * Do the task corresponding to the given task on the given assemblyline.
+	 * @param Task				The task that needs to be completed wrapped in the printable interface.
+	 * @param assemblyLine		The assemblyline that the task is on wrapped in the printable interface.
+	 * @return
+	 */
+	public boolean doTask(Printable Task, Printable assemblyLine){
+		AssemblyLine line = this.get(assemblyLine);
+		return line.doTask(Task);
+	}
+
 	
+	
+	public boolean advance(){
+		for(AssemblyLine al : assemblyLines){
+			
+		}
+	}
+	
+	/**
+	 * Will get the correct assemblyline matching the one encapsulated within the parameter or null 
+	 * @param line
+	 * @return
+	 */
+	private AssemblyLine get(Printable line){
+		for(AssemblyLine al : assemblyLines){
+			if(al.equals(line)){
+				return al;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Add an assembly line to this scheduler.
+	 * @param line
+	 */
+	private void addLine(AssemblyLine line){
+		assemblyLines.add(line);
+	}
+
 	private void initializeAssemblylines(){
-		WorkstationDirector director = new WorkstationDirectorA(new WorkstationChainBuilder());
-		this.addLine(new AssemblyLine(capabilities, director));
-		
-		
-		
-		
-		WorkstationChainBuilder builder = new WorkstationChainBuilder();
-		WorkstationDirector director = new WorkstationDirector(builder);
+		buildLine1();
+		buildLine2();
+		buildLine3();
+	}
+	
+	/**
+	 * NOTE: BUILDER PATTERN MUCH?
+	 */
+	
+
+	private void buildLine1(){
+		WorkstationChainBuilder builder = new WorkstationChainBuilder();	
+		WorkstationDirector director = new WorkstationDirectorA(builder);
 		director.construct();
-		this.firstWorkStation = builder.getResult();
+		
+		VehicleModel[] models = {VehicleModel.CARMODELA, VehicleModel.CARMODELB};
+		this.addLine(new AssemblyLine(Arrays.asList(models), builder));
+	}
+	
+	private void buildLine2() {
+		WorkstationChainBuilder builder = new WorkstationChainBuilder();	
+		WorkstationDirector director = new WorkstationDirectorA(builder);
+		director.construct();
+		
+		VehicleModel[] models = {VehicleModel.CARMODELA, VehicleModel.CARMODELB, VehicleModel.CARMODELC};
+		this.addLine(new AssemblyLine(Arrays.asList(models), builder));
+	}
+	
+	private void buildLine3() {
+		WorkstationChainBuilder builder = new WorkstationChainBuilder();	
+		WorkstationDirector director = new WorkstationDirectorB(builder);
+		director.construct();
+		
+		VehicleModel[] models = {VehicleModel.CARMODELA, VehicleModel.CARMODELB, VehicleModel.CARMODELC, VehicleModel.TRUCKMODELX, VehicleModel.TRUCKMODELY};
+		this.addLine(new AssemblyLine(Arrays.asList(models), builder));
 	}
 	
 	
