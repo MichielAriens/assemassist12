2a417ba5ec2379f2693dc463dc57d29ef3e3d57b
JeroenCo <jeroen.compagnie@student.kuleuven.be>
2014-05-19 16:31:34 +0200

    delay docu

--- javaprojectassem12/src/logic/assemblyline/Delay.java
+++ javaprojectassem12/src/logic/assemblyline/Delay.java
@@ -3,40 +3,40 @@ package logic.assemblyline;
 import org.joda.time.DateTime;
 
 /**
- *
+ * A class representing delays that happened on certain dates. 
  */
 public class Delay implements Comparable<Delay> {
 	/**
-	 * 
+	 * How long the delay was in minutes.
 	 */
 	int delayTime;
 	
 	/**
-	 * 
+	 * The date the delay took place.
 	 */
 	DateTime dateOfDelay;
 	
 	/**
-	 * 
-	 * @param delay
-	 * @param dateOfDelay
+	 * Constructs a new delay with the given delay time and date of the delay.
+	 * @param delayTime	How long the delay was in minutes.
+	 * @param dateOfDelay	The date the delay took place.
 	 */
-	public Delay (int delay, DateTime dateOfDelay){
-		this.delayTime = delay;
+	public Delay (int delayTime, DateTime dateOfDelay){
+		this.delayTime = delayTime;
 		this.dateOfDelay = dateOfDelay;
 	}
 
 	/**
-	 * 
-	 * @return
+	 * Returns how long the delay was in minutes.
+	 * @return how long the delay was in minutes
 	 */
 	public int getDelayTime(){
 		return delayTime;
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Returns the date of when the delay took place.
+	 * @return the date of when the delay took place.
 	 */
 	public DateTime getDateOfDelay() {
 		return dateOfDelay;
