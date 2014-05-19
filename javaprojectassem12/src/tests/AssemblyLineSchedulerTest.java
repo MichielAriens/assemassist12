3cc9bc740d3e0b3f2dc0e81abd03f3d709b98207
Michiel Ariens <michiel.ariens@student.kuleuven.be>
2014-05-19 14:46:32 +0200

    test

--- javaprojectassem12/src/tests/AssemblyLineSchedulerTest.java
+++ javaprojectassem12/src/tests/AssemblyLineSchedulerTest.java
@@ -1,6 +1,10 @@
 package tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import logic.car.CarOrderDetailsMaker;
 import logic.car.Order;
 import logic.car.VehicleModel;
@@ -8,6 +12,7 @@ import logic.car.VehicleOrder;
 import logic.car.VehiclePart;
 import logic.users.CarManufacturingCompany;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 public class AssemblyLineSchedulerTest {
@@ -41,10 +46,34 @@ public class AssemblyLineSchedulerTest {
 	
 	@Test
 	public void testBasicOrder(){
-		Order order = buildStandardOrderA();
+		List<Order> orders = new ArrayList<>(3);
+		for(int i = 0; i < 4 ; i++){
+			orders.add(buildStandardOrderA());
+		}
 		CarManufacturingCompany cmc = new CarManufacturingCompany();
-		cmc.addOrder(order);
-		System.out.println("lol");
+		for(Order order : orders){
+			cmc.addOrder(order);
+		}
+		DateTime est = cmc.getCurrentTime();
+		est = est.plusMinutes(150);
+		for(Order order : orders){
+			assertTrue(eqiDateTime(order.getEstimatedEndTime(),est));
+		}
+		
+	}
+	
+	/**
+	 * Tests whether the two DateTime object provided describe the same moment in time accurate to the minute. 
+	 */
+	public static boolean eqiDateTime(DateTime a, DateTime b){
+		if(a.getYear() == b.getYear()){
+			if(a.getDayOfYear() == b.getDayOfYear()){
+				if(a.getMinuteOfDay() == b.getMinuteOfDay()){
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 	
 
